742d5c7f9c1c1bd8f0a348d42d120cd0944a4f80
RuvenPinkhasov <rubi00010@gmail.com>
2024-01-13 08:12:44 -0500

    created the FormValidator constructor

--- components/FormValidator.js
+++ components/FormValidator.js
@@ -1,4 +1,18 @@
 export default class FormValidator {
   //------------------FORM VALIDATOR CONSTRUCTOR--------------------------->>
-  constructor() {}
+  constructor(
+    formSelector,
+    inputSelector,
+    submitButtonSelector,
+    inactiveButtonClass,
+    inputErrorClass,
+    errorClass
+  ) {
+    this._formSelector = formSelector;
+    this._inputSelector = inputSelector;
+    this._submitButtonSelector = submitButtonSelector;
+    this._inactiveButtonClass = inactiveButtonClass;
+    this._inputErrorClass = inputErrorClass;
+    this._errorClass = errorClass;
+  }
 }
