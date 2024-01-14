d768a79e0183fc83c38a7eab89f82e5927c511f8
RuvenPinkhasov <rubi00010@gmail.com>
2024-01-14 07:21:31 -0500

    added comments on formvalidator.js

--- components/FormValidator.js
+++ components/FormValidator.js
@@ -18,18 +18,21 @@ export default class FormValidator {
     this._errorClass = errorClass;
   }
 
+  //-----------METHOD THAT SHOWS THE INPUT ERROR-------------->>
   _showInputError = (inputElement) => {
     inputElement.classList.add(this._inputErrorClass);
     this._errorElement.textContent = inputElement.validationMessage;
     this._errorElement.classList.add(this._errorClass);
   };
 
+  //-----------METHOD THAT HIDES THE INPUT ERROR-------------->>
   _hideInputError = (inputElement) => {
     inputElement.classList.remove(this._inputErrorClass);
     this._errorElement.classList.remove(this._errorClass);
     this._errorElement.textContent = "";
   };
 
+  //-----------METHOD THAT CHECKS EACH ELEMENT FOR VALIDITY-------------->>
   _checkInputValidity = (inputElement) => {
     this._errorElement = this._formElement.querySelector(
       `#${inputElement.id}-error`
@@ -41,22 +44,24 @@ export default class FormValidator {
     }
   };
 
+  //-----------METHOD TO CHECK IF ALL INPUT ELEMENTS ARE VALID-------------->>
   _hasInvalidInput = (inputList) => {
     return inputList.some((inputElement) => {
       return !inputElement.validity.valid;
     });
   };
 
+  //-----------METHOD THAT TOGGLES THE SUBMIT BUTTON-------------->>
   _toggleButtonState = (inputList) => {
     if (this._hasInvalidInput(inputList)) {
-      this._buttonElement.classList.add(this._inactiveButtonClass);
-      this._buttonElement.setAttribute("disabled", true);
+      this.disableSubmit();
     } else {
       this._buttonElement.classList.remove(this._inactiveButtonClass);
       this._buttonElement.removeAttribute("disabled");
     }
   };
 
+  //-----------METHOD THAT SETS EVENT LISTENERS ON THE INPUT ELEMENTS-------------->>
   _setEventListeners() {
     const inputList = Array.from(
       this._formElement.querySelectorAll(this._inputSelector)
@@ -73,11 +78,13 @@ export default class FormValidator {
     });
   }
 
+  //------------------------METHOD TO ENABLE VALIDATION----------------------->>
   enableValidation() {
     this._formElement = document.querySelector(this._formSelector);
     this._setEventListeners();
   }
 
+  //-----------------METHOD TO DIABLE THE SUBMIT BUTTON------------------>>
   disableSubmit() {
     this._buttonElement.classList.add(this._inactiveButtonClass);
     this._buttonElement.setAttribute("disabled", true);
