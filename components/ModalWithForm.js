bac46c45bf94175a959e003052a1c1ff79036499
RuvenPinkhasov <rubi00010@gmail.com>
2024-01-26 16:59:34 -0500

    started construction of ModalWithForm Class

--- components/ModalWithForm.js
+++ components/ModalWithForm.js
@@ -1,1 +1,12 @@
 import Modal from "./Modal.js";
+
+export default class ModalWithForm extends Modal {
+  constructor(modalSelector, formSubmit) {
+    super(modalSelector);
+    this._formSubmit = formSubmit;
+  }
+
+  open() {
+    super.open();
+  }
+}
