4999961c516ea780d22ee074bad12699009a5b73
RuvenPinkhasov <rubi00010@gmail.com>
2024-01-26 00:13:32 -0500

    started ModalWithImage class

--- components/ModalWithImage.js
+++ components/ModalWithImage.js
@@ -1,1 +1,15 @@
 import Modal from "./Modal.js";
+
+export default class ModalWithImage extends Modal {
+  constructor(modalSelector) {
+    super(modalSelector);
+  }
+
+  opend({ name, link }) {
+    this._previewImage = modalImagePreview.querySelector(".modal__image");
+    previewImage.setAttribute("src", link);
+    previewImage.setAttribute("alt", name);
+    previewImageTitle.textContent = name;
+    super.open();
+  }
+}
