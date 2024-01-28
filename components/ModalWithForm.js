4b6e1223793dcde1ff5288cd90b3103616a23737
RuvenPinkhasov <rubi00010@gmail.com>
2024-01-28 12:10:39 -0500

    finished ModalWithForm Class

--- components/ModalWithForm.js
+++ components/ModalWithForm.js
@@ -1,14 +1,26 @@
 import Modal from "./Modal.js";
 
 export default class ModalWithForm extends Modal {
-  constructor(modalSelector, formSubmit) {
+  constructor(modalSelector, formSubmit, { formSelector }) {
     super(modalSelector);
+    this._form = this._modal.querySelector(formSelector);
     this._formSubmit = formSubmit;
   }
 
-  _getInputValues = () => {};
+  _getInputValues() {
+    const inputList = Array.from(this._form.querySelectorAll(".modal__input"));
+    const inputValues = {};
+    inputList.forEach((input) => {
+      inputValues[input.name] = input.value;
+    });
+    return inputValues;
+  }
 
   setEventListeners = () => {
+    this._form.addEventListener("submit", () => {
+      const values = this._getInputValues();
+      this._formSubmit(values);
+    });
     super.setEventListeners();
   };
 }
