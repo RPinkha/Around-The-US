b13132cc9a48189bfd9e3c0706f48546e337c40d
RuvenPinkhasov <rubi00010@gmail.com>
2024-02-08 21:15:44 -0500

    finsihed Api constructor and started building get cards method

--- src/components/Api.js
+++ src/components/Api.js
@@ -1,3 +1,27 @@
 export default class Api {
-  constructor(options) {}
+  constructor(options) {
+    this._baseUrl = options.baseUrl;
+    this._authorization = options.headers.authorization;
+    this._contentType = options.headers.contentType;
+  }
+
+  getInitialCards() {
+    const initialCards = [];
+    return fetch("https://around-api.en.tripleten-services.com/v1", {
+      headers: {
+        authorization: "739decfe-71be-4d0d-8961-a71a0f6cba52",
+      },
+    })
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        }
+        return Promise.reject(`Error: ${res.status}`);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
 }
+
+/// My autherization Token: 739decfe-71be-4d0d-8961-a71a0f6cba52
