79ecd9ba0463679af2ab679e737cb2817c633de3
RuvenPinkhasov <rubi00010@gmail.com>
2024-02-11 22:41:41 -0500

    added ModalWithConfirmation Class

---
+++ src/components/ModalWithConfirmation.js
@@ -0,0 +1,19 @@
+import Modal from "./Modal.js";
+
+export default class ModalWithForm extends Modal {
+  //---------------MODALWITHFORM CONSTRUCTOR------------------------>>
+  constructor(modalSelector, formSubmit, { formSelector }) {
+    super(modalSelector);
+    this._form = this._modalElement.querySelector(formSelector);
+    this._formSubmit = formSubmit;
+  }
+
+  //--------ADDS FUNCTIONALITY TO THE SETEVENTLISTENERS METHOD---------->>
+  setEventListeners() {
+    this._form.addEventListener("submit", (evt) => {
+      evt.preventDefault();
+      this._formSubmit(values);
+    });
+    super.setEventListeners();
+  }
+}
